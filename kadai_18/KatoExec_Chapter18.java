fb684e95b9a7552e632ee1f3eca14b943299f07b
kazuma-sasaki23 <kusuoman.07@gmail.com>
2025-02-03 23:14:42 +0900

    Add files via upload

---
+++ kadai_18/KatoExec_Chapter18.java
@@ -0,0 +1,22 @@
+package kadai_18;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+       
+		KatoTaro_Chapter18 KatoTaro = new KatoTaro_Chapter18();
+		KatoTaro.setGivenName();
+		KatoTaro.execIntroduce();
+		
+		KatoIchiro_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
+		KatoIchiro.setGivenName();
+		KatoIchiro.execIntroduce();
+		
+		KatoHanako_Chapter18 KatoHanako = new KatoHanako_Chapter18();
+		KatoHanako.setGivenName();
+		KatoHanako.execIntroduce();
+				
+	}
+
+}
